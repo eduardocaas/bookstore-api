ce4653d3ba158f2835a6da4b7087cf738f302508
Eduardo Castro <eduardofc.dev@gmail.com>
2023-01-30 23:07:21 -0300

    findById para categoria

---
+++ src/main/java/com/efc/bookstoreapi/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.efc.bookstoreapi.services;
+
+import com.efc.bookstoreapi.domain.Categoria;
+import com.efc.bookstoreapi.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repo;
+
+    public Categoria findById(Integer id) {
+
+        Optional<Categoria> obj = repo.findById(id);
+        return obj.orElse(null);
+    }
+
+}
