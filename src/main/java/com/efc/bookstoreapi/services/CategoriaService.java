fb5769dd912da49dbbf3c17453dae2a077881f28
Eduardo Castro <eduardofc.dev@gmail.com>
2023-01-31 15:25:40 -0300

    Método findAll CategoriaService

--- src/main/java/com/efc/bookstoreapi/services/CategoriaService.java
+++ src/main/java/com/efc/bookstoreapi/services/CategoriaService.java
@@ -6,6 +6,7 @@ import com.efc.bookstoreapi.repositories.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,6 +19,10 @@ public class CategoriaService {
 
         Optional<Categoria> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id + ", Tipo: " + Categoria.class.getName()));
+    }
+
+    public List<Categoria> findAll() {
+        return repo.findAll();
     }
 
 }
