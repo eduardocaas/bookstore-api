8320f70cdc1e6c0f291b9b6ab6a87143f37e66be
Eduardo Castro <104233554+eduardocaas@users.noreply.github.com>
2023-03-23 15:44:33 -0300

    Configuração de CORS para webpage na nuvem

--- src/main/java/com/efc/bookstoreapi/config/CorsConfiguration.java
+++ src/main/java/com/efc/bookstoreapi/config/CorsConfiguration.java
@@ -15,7 +15,7 @@ public class CorsConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("https://bookstore-eta-eight.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD");
             }
         };
