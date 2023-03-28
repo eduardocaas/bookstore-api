b75deafa1145b1501c5f25fd6d787fa5f25aa8d1
Eduardo Castro <104233554+eduardocaas@users.noreply.github.com>
2023-03-28 09:34:04 -0300

    webpage na nuvem

--- src/main/java/com/efc/bookstoreapi/config/CorsConfiguration.java
+++ src/main/java/com/efc/bookstoreapi/config/CorsConfiguration.java
@@ -14,8 +14,9 @@ public class CorsConfiguration {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                registry.addMapping("/**") 
+                        .allowedOrigins("https://bookstore-eta-eight.vercel.app")
+                        //.allowedOrigins("http://localhost:4200")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD");
             }
         };
